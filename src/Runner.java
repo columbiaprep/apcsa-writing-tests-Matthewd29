6328a61c25a2861ad8ad3d04a9a62bd7d23d20b0
Matthewd29 <mdieterich26@cgp.sorg>
2025-01-08 11:54:02 -0500

    lab

--- src/Runner.java
+++ src/Runner.java
@@ -8,6 +8,31 @@ public class Runner {
 
     @Test
     public void test() {
-        assertEquals(3, 1+1);
+        Helmet h = new Helmet("Gold");
+        assertEquals(1.5, h.getWeight(), .1);
+
+        Gauntlets g = new Gauntlets();
+        assertEquals(0,g.getEquipmentID());
+
+        Gloves gl = new Gloves();
+        assertEquals(1500, gl.getGloveCost());
+
+        Boots b = new Boots();
+        assertEquals(0, b.getRevisedStrength());
+
+        BodyArmor ba = new BodyArmor();
+        assertEquals(0, ba.getMaterialType());
+
+        MeleeWeapon m = new MeleeWeapon();
+        assertEquals(0, m.getDamage());
+
+        Role r = new Role();
+        assertEquals(0, r.getName());
+
+        Shield s = new Shield();
+        assertEquals(0, s.getMaterial());
+
+        Species sp = new Species();
+        assertEquals(0, sp.getCanFly());
     }
 }
